9ede4b16939f5c5021a69151a20588ab01c78e12
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2022-06-29 14:44:56 +0430

    auto-commit

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const db = require('./database');
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const db = require("./database/database");
+
+//middle wares
+app.use(express.json());
+
+const port = process.env.PORT
+app.listen(port,console.log(`Server is listening on port ${port}`));
